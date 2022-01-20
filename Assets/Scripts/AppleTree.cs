abb0dcde6e98b9356a08bf742b8736bd295e6b03
Keegan McGonigle <keegantmcgonigle@gmail.com>
2022-01-20 00:17:23 -0700

    Added Apple Spawning

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [SerializeField] private GameObject _applePrefab;
+    public float spawnRadius = 8;
+    public float rate;
+    float spawnTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTimer = rate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
+        {
+
+                Vector2 randomPoint = Random.insideUnitCircle.normalized;
+            Vector3 spawnPoint = new Vector3(spawnRadius * randomPoint.x, 3, spawnRadius * randomPoint.y);
+                SpawnAppleAtPosition(transform.position + spawnPoint);
+
+            spawnTimer = rate;
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    //Vector3 mousePos = Input.mousePosition;
+        //    //Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        //    //if(Physics.Raycast(ray, out RaycastHit hitInfo))
+        //    //{
+        //    //    SpawnAppleAtPosition(hitInfo.point);
+        //    //}
+        //    Vector2 randomPoint = Random.insideUnitCircle.normalized;
+        //    SpawnAppleAtPosition(transform.position + new Vector3(spawnRadius*randomPoint.x, 3, spawnRadius * randomPoint.y));
+        //}
     }
+
+    private void SpawnAppleAtPosition(Vector3 spawnPosition)
+    {
+        GameObject apple = Instantiate(_applePrefab, spawnPosition, Quaternion.identity);
+        apple.transform.Rotate(Random.rotation.eulerAngles);
+    }
+
 }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -8,13 +8,17 @@ public class Basket : MonoBehaviour
     public AppleTree spawner;
 
     public TextMesh counter;
-    private int applesCollected = 0;
-    
 
-    // Start is called before the first frame update
-    void Start()
+    private int applesCollected = 0;
+    private Vector3 center;
+    //[SerializeField]
+    //private Transform leftArrow;
+    //[SerializeField]
+    //private Transform rightArrow;
+  // Start is called before the first frame update
+  void Start()
     {
-        
+        center = spawner.transform.position - Vector3.up * spawner.transform.position.y;
     }
 
 
@@ -30,14 +34,38 @@ public class Basket : MonoBehaviour
         if(other.tag == "apple")
         {
             Destroy(other.gameObject);
-            counter.text = applesCollected++.ToString();
+            applesCollected++;
+            counter.text = applesCollected.ToString();
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector3 nearestApple = GameObject.FindGameObjectWithTag("apple").transform.position;
+
+        //float leftDistance = (nearestApple - leftArrow.position).magnitude;
+        //float rightDistance = (nearestApple - rightArrow.position).magnitude;
+
+        //if (leftDistance < rightDistance)
+        //{
+        //    leftArrow.localScale = Vector3.one * Mathf.Clamp(Mathf.Pow(leftDistance+.5f,2), 5, 35);
+        //    rightArrow.localScale = Vector3.zero;
+        //}
+        //else
+        //{
+        //    rightArrow.localScale = Vector3.one * Mathf.Clamp(Mathf.Pow(rightDistance+.5f, 2), 5, 35);
+        //    leftArrow.localScale = Vector3.zero;
+        //}
+
+
+
+        //var forwardMovement = Input.GetAxis("Vertical");
         transform.LookAt(spawner.transform.position-Vector3.up*spawner.transform.position.y+ Vector3.up *transform.position.y);
+        
+        //transform.Translate(Vector3.forward * forwardMovement * Time.deltaTime * speed);
+        
+        
+        
         var moveDirection = Input.GetAxis("Horizontal");
         transform.Translate( Vector3.right*moveDirection* Time.deltaTime*speed);
     }
